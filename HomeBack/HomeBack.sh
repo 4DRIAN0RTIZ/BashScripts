5b6df5c965e8f1fa842290a2c12e07808e14c59c
Red4DR <2121100617@soy.utj.edu.mx>
2023-02-22 09:51:06 -0600

    HomeBackv1.1_ForCron

--- HomeBack/HomeBack.sh
+++ HomeBack/HomeBack.sh
@@ -1,78 +1,86 @@
 #!/bin/bash
-# Verifica si las dependencias estan instaladas y las instala si no lo estan
+echo -e "\a"
+# Verificación de dependencias
 
-if ! [ -x "$(command -v python3)" ]; then
-  echo 'Python3 no esta instalado. Instalando...'
-  sudo apt install python-is-python3
-fi
-
-if ! [ -f "HomeBack.py" ]; then
-  echo 'No se encuentra el script de python. Descargando...'
-  wget https://gist.githubusercontent.com/4DRIAN0RTIZ/e880ec9ac49b73531514d5861a302be9/raw/d440b9175f5a9ac24023d8eee8c39817279b3f36/HomeBack.py
-  wget https://gist.githubusercontent.com/4DRIAN0RTIZ/45c6273ebe20efeaf02c53ec156fd417/raw/63209dc6b5ac5cb2f870bfddcc0d6412bccded21/settings.yaml
-  wget https://gist.githubusercontent.com/4DRIAN0RTIZ/5da96f80dd35f81d596b0f95517ed838/raw/bb106e6e7efa3a5733245bbcb9a033783147f7ca/gdrive.py
-  echo 'Descarga completa'
-fi
-
-excluidos=("/home/$(whoami)/.home-backup" "/home/$(whoami)/.cache" "/home/$(whoami)/.local/share/Trash" "/home/$(whoami)/.local/share/Trash/files" "/home/$(whoami)/.local/share/Trash/info" "/home/$(whoami)/.local/share/Trash/expunged" "/home/$(whoami)/snap/")
-origen="$HOME/University/IoT"
-destino="$HOME/.home-backup/"
-fecha_actual=$(date "+%Y-%m-%d-%H-%M-%S")
-nombre_archivo="backup-${fecha_actual}-$(whoami).tar.gz"
-
-# Crea el directorio de destino si no existe
-
-if [ ! -d "${destino}" ] 
-then
-    mkdir -p "${destino}"
-fi
-
-rm -rf $destino/*
-
-# Itera sobre los archivos y directorios del origen y los copia al destino
-
-n_archivos=$(ls -1 ${origen}/* | wc -l)
-contador=0
-
-echo "Copiando archivos..."
-
-for archivo in "${origen}"/*
-do
-    if [ -d "${archivo}" ] && [[ " ${excluidos[@]} " =~ " $(basename "${archivo}") " ]] 
-    then
-        echo "Excluyendo: $(basename "${archivo}")"
-    else
-        cp -r "${archivo}" "${destino}"
+declare -A dependencies=(
+  ["wget"]="sudo apt install wget -y"
+  ["tar"]="sudo apt install tar -y"
+  ["python3"]="sudo apt install python-is-python3 -y"
+)
+for dependency in "${!dependencies[@]}"; do
+  if ! command -v "$dependency" &> /dev/null; then
+    echo "El comando '$dependency' no está instalado"
+    echo "Instalando '$dependency'..."
+    eval "${dependencies[$dependency]}"
+    if [ $? -ne 0 ]; then
+      echo "Error al instalar '$dependency'"
+      exit 1
     fi
-
-    contador=$((contador+1))
-    porcentaje=$((contador*100/${n_archivos}))
-    echo -ne "\r[${porcentaje}%] ["
-    for i in $(seq 1 $((porcentaje/2)))
-    do
-        echo -ne "#"
-    done
-    for i in $(seq 1 $((porcentaje/2+1)) 50)
-    do
-        echo -ne " "
-    done
-    echo -ne "]"
-
-    # ajusta el tiempo de espera según tus necesidades
-    sleep 0.2
+  else
+    echo "$dependency ya está instalado"
+  fi
 done
-echo " "
 
-# Se comprime el directorio de destino en un archivo .tar.gz
+# Verificación de archivos en el directorio
 
- echo "Comprimiendo archivos..."
- tar -czf "$HOME/.home-backup/${nombre_archivo}" ${destino} 1>/dev/null 2>&1
- echo "Copia de seguridad creada: ${nombre_archivo}"
- echo "Guardada en: ${destino}"
- sleep 2
+declare -A files_to_download=(
+  ["settings.yaml"]="https://gist.githubusercontent.com/4DRIAN0RTIZ/45c6273ebe20efeaf02c53ec156fd417/raw/63209dc6b5ac5cb2f870bfddcc0d6412bccded21/settings.yaml"
+  ["gdrive.py"]="https://gist.githubusercontent.com/4DRIAN0RTIZ/5da96f80dd35f81d596b0f95517ed838/raw/bb106e6e7efa3a5733245bbcb9a033783147f7ca/gdrive.py"
+  ["HomeBack.py"]="https://gist.githubusercontent.com/4DRIAN0RTIZ/e880ec9ac49b73531514d5861a302be9/raw/d440b9175f5a9ac24023d8eee8c39817279b3f36/HomeBack.py"
+)
 
-# Se ejecuta el script de python para enviar el archivo a google drive
+for file in "${!files_to_download[@]}"; do
+  if [ ! -f "$file" ]; then
+    echo "Archivo $file no encontrado"
+    echo "Descargando $file..."
+    wget "${files_to_download[$file]}"
+    if [ $? -ne 0 ]; then
+      echo "Error al descargar $file"
+      exit 1
+    fi
+  else
+    echo "$file ya está descargado"
+  fi
+done
 
-echo "Ejecutando script de python..."
-python3 HomeBack.py "${nombre_archivo}"
+# Ruta de  del archivo de backup
+backup_dir="$HOME/.backup"
 
+# Carpetas a excluir en la copia de seguridad
+excluded_folders=(
+    # "$HOME/.cache"
+    # "$HOME/.bin"
+    # "$HOME/snap"
+    # "$HOME/.mozilla"
+    # "$HOME/.backup"
+    # "$HOME/.local"
+)
+
+# Crear directorio de backup si no existe
+if [ ! -d "$backup_dir" ]; then
+    mkdir -p "$backup_dir"
+fi
+
+echo "Eliminando archivos de backup antiguos"
+# Eliminar archivos de backup antiguos
+find "$backup_dir" -type f -delete
+echo "Fin de eliminación de archivos de backup antiguos"
+
+# Nombre del archivo de backup
+backup_file="backup_home_$(date +%Y%m%d_%H%M%S).tar.gz"
+
+# Crear archivo de backup y comprimirlo en el directorio de destino
+tar_cmd="tar -czf $backup_dir/$backup_file $HOME/University/IoT"
+
+
+for folder in "${excluded_folders[@]}"; do
+    tar_cmd+=" --exclude=$folder"
+done
+
+
+ eval "$tar_cmd"
+
+ echo "Backup completado: $backup_dir/$backup_file"
+
+  echo "Abriendo Script de Python"
+  python3 HomeBack.py $backup_file
